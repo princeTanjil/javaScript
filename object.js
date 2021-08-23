70310797b78ee75c308696962f10296e7c621314
princeTanjil <princetanjil14102026@gmail.com>
2021-08-23 13:16:37 +0600

    object access here

---
+++ object.js
@@ -0,0 +1,13 @@
+//accessing a object
+
+var obj ={
+    a:10,
+    b:80,
+    c:20
+}
+console.log(obj.a)
+console.log(obj.b)
+console.log(obj.a + obj.b)
+// array notation a object print 
+
+console.log(obj['a'])
